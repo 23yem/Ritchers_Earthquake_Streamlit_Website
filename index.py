79d8844d49dad076f9829d79e48155ab2fbc2325
Michael Ye <michaelye22@gmail.com>
2024-01-21 19:11:45 -0500

    feat: add structure to home page

--- index.py
+++ index.py
@@ -2,3 +2,9 @@ import streamlit as st
 from streamlit.components import v1 as components
 
 st.set_page_config(layout="wide")
+
+# Load HTML file
+with open('index.html', 'r') as file:    
+    html_content = file.read()
+
+components.html(html_content, width = None, height=2300)
